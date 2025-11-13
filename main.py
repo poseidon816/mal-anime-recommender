92a5b51009d1c16cf0055a85ce96747ec80f8624
Harish Mogaveer <61499169+poseidon816@users.noreply.github.com>
2025-11-13 21:21:10 +0530

    Update main.py port number change
    Because railway was choosing 8080 port so i made the python to listen to 8080 port

--- main.py
+++ main.py
@@ -359,4 +359,4 @@ async def post_recommendations(request: RecommendationRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
